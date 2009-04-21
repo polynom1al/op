412420135094bb5779ac28858cfe4f148d914576
Alex Ayars <gitspam@nodekit.org>
2009-04-20 18:43:26 -0700

    add support for app-specific rc files

--- lib/OP/Constants.pm
+++ lib/OP/Constants.pm
@@ -26,6 +26,15 @@ be located under C<$ENV{OP_HOME}>, which defaults to C</opt/op>.
 An example C<.oprc> is included in the top level directory of this
 distribution, and also given later in this document.
 
+=head1 SECURITY
+
+The <.oprc> file represents the keys to the kingdom.
+
+Treat your <.oprc> file with the same degree of lockdown as you would
+with system-level executables and their associated configuration
+files. It should not be kept in a location where untrusted parties
+can write to it, or where any unaudited changes can occur.
+
 =head1 SYNOPSIS
 
 To import constants directly, just specify them when using OP::Constants:
@@ -86,81 +95,68 @@ our @EXPORT_OK;
 # Default RC file location if OP_HOME is not set
 #
 use constant DefaultPath => '/opt/op';
-use constant RC => '.oprc';
 
-$ENV{OP_HOME} ||= DefaultPath;
+sub init {
+  my $RC = shift;
+  my $caller = caller;
 
-#
-# Default exported values if RC file is not usable
-#
-use constant DefaultYamlRoot => '/tmp/yaml';
-use constant DefaultSqliteRoot => '/tmp/sqlite';
-use constant DefaultScratchRoot => '/tmp/scratch';
-use constant DefaultDbName => 'op';
-use constant DefaultDbHost => 'localhost';
-use constant DefaultDbPass => '~';
-use constant DefaultDbPort => '3306';
-use constant DefaultDbUser => 'op';
-use constant DefaultMemcacheHosts => [ '127.0.0.1:31337' ];
-use constant DefaultUrlRoot => 'http://127.0.0.1/';
-use constant DefaultRcsBindir => '/usr/bin';
-use constant DefaultRcsDir => 'RCS';
+  $ENV{OP_HOME} ||= DefaultPath;
 
-#
-# Private package variables
-#
-my $rc;
+  #
+  # Private package variables
+  #
+  my $rc;
 
-my $path = join('/',$ENV{OP_HOME},RC);
+  my $path = join('/',$ENV{OP_HOME},$RC);
 
-my $override = join('/',$ENV{OP_HOME},hostname(),RC);
+  my $override = join('/',$ENV{OP_HOME},hostname(),$RC);
 
-if ( $ENV{OP_HOME} && -f $path ) {
-  my $file = IO::File->new($path, 'r')
-    || throw OP::FileAccessError("Could not read $path: $@");
+  if ( $ENV{OP_HOME} && -f $path ) {
+    my $file = IO::File->new($path, 'r')
+      || throw OP::FileAccessError("Could not read $path: $@");
 
-  my @yaml;
+    my @yaml;
 
-  while ( <$file> ) { push @yaml, $_ }
-
-  $file->close();
-
-  eval {
-    $rc = YAML::Syck::Load( join('', @yaml) ) || die $@;
-  };
-
-  throw OP::RuntimeError($@) if $@;
-
-  throw OP::RuntimeError("Unexpected format in $path: Should be a HASH")
-    if !ref($rc) || ref($rc) ne 'HASH';
-
-  if ( -f $override ) {
-    my $file = IO::File->new($override, 'r')
-      || throw OP::FileAccessError("Could not read $override: $@");
-
-    my @overrideYAML;
-
-    while ( <$file> ) { push @overrideYAML, $_ }
+    while ( <$file> ) { push @yaml, $_ }
 
     $file->close();
 
-    my $overlay;
-
     eval {
-      $overlay = YAML::Syck::Load( join('', @overrideYAML) ) || die $@;
+      $rc = YAML::Syck::Load( join('', @yaml) ) || die $@;
     };
 
     throw OP::RuntimeError($@) if $@;
 
-    throw OP::RuntimeError("Unexpected format in $override: Should be a HASH")
-      if !ref($overlay) || ref($overlay) ne 'HASH';
+    throw OP::RuntimeError("Unexpected format in $path: Should be a HASH")
+      if !ref($rc) || ref($rc) ne 'HASH';
 
-    for my $key ( keys %{ $overlay } ) {
-      $rc->{$key} = $overlay->{$key};
+    if ( -f $override ) {
+      my $file = IO::File->new($override, 'r')
+        || throw OP::FileAccessError("Could not read $override: $@");
+
+      my @overrideYAML;
+
+      while ( <$file> ) { push @overrideYAML, $_ }
+
+      $file->close();
+
+      my $overlay;
+
+      eval {
+        $overlay = YAML::Syck::Load( join('', @overrideYAML) ) || die $@;
+      };
+
+      throw OP::RuntimeError($@) if $@;
+
+      throw OP::RuntimeError("Unexpected format in $override: Should be a HASH")
+        if !ref($overlay) || ref($overlay) ne 'HASH';
+
+      for my $key ( keys %{ $overlay } ) {
+        $rc->{$key} = $overlay->{$key};
+      }
     }
-  }
-} else {
-  print STDERR q(!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  } else {
+    print STDERR q(!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     _  _____ _____ _____ _   _ _____ ___ ___  _   _ 
    / \|_   _|_   _| ____| \ | |_   _|_ _/ _ \| \ | |
@@ -171,58 +167,97 @@ if ( $ENV{OP_HOME} && -f $path ) {
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 );
 
-  my $current = $ENV{OP_HOME} || "";
+    my $current = $ENV{OP_HOME} || "";
                                                     
-  print STDERR "\n";
-  print STDERR "\"$0\" needs some help finding a file.\n";
-  print STDERR "\n";
-  print STDERR "HERE'S WHAT'S WRONG:\n";
-  print STDERR "  OP can't find a valid constants file, \".oprc\"!\n";
-  print STDERR "  .oprc lives under OP_HOME (currently: \"$current\").\n";
-  print STDERR "\n";
-  print STDERR "HOW TO FIX THIS:\n";
-  print STDERR "  Set OP_HOME to the location of a valid .oprc, in the\n";
-  print STDERR "  shell environment or calling script. For example:\n";
-  print STDERR "\n";
-  print STDERR "  In bash:\n";
-  print STDERR "    export OP_HOME=\"/path/to\"\n";
-  print STDERR "\n";
-  print STDERR "  Or in $0:\n";
-  print STDERR "    \$ENV{OP_HOME} = \"/path/to\";\n";
-  print STDERR "\n";
-  print STDERR "This must be corrected before proceeding.\n";
-  print STDERR "\n";
-  print STDERR "A starter .oprc should have accompanied this\n";
-  print STDERR "distribution. An example is also given on the\n";
-  print STDERR "OP::Constants manual page.\n";
-  print STDERR "\n";
+    print STDERR "\n";
+    print STDERR "\"$0\" needs some help finding a file.\n";
+    print STDERR "\n";
+    print STDERR "HERE'S WHAT'S WRONG:\n";
+    print STDERR "  OP can't find a valid constants file, \".oprc\"!\n";
+    print STDERR "  .oprc lives under OP_HOME (currently: \"$current\").\n";
+    print STDERR "\n";
+    print STDERR "HOW TO FIX THIS:\n";
+    print STDERR "  Set OP_HOME to the location of a valid .oprc, in the\n";
+    print STDERR "  shell environment or calling script. For example:\n";
+    print STDERR "\n";
+    print STDERR "  In bash:\n";
+    print STDERR "    export OP_HOME=\"/path/to\"\n";
+    print STDERR "\n";
+    print STDERR "  Or in $0:\n";
+    print STDERR "    \$ENV{OP_HOME} = \"/path/to\";\n";
+    print STDERR "\n";
+    print STDERR "This must be corrected before proceeding.\n";
+    print STDERR "\n";
+    print STDERR "A starter .oprc should have accompanied this\n";
+    print STDERR "distribution. An example is also given on the\n";
+    print STDERR "OP::Constants manual page.\n";
+    print STDERR "\n";
 
-  exit(2);
+    exit(2);
+  }
 
-  $rc = {
-    yamlRoot => DefaultYamlRoot,
-    sqliteRoot => DefaultSqliteRoot,
-    scratchRoot => DefaultScratchRoot,
-    dbName => DefaultDbName,
-    dbHost => DefaultDbHost,
-    dbPass => DefaultDbPass,
-    dbPort => DefaultDbPort,
-    dbUser => DefaultDbUser,
-    memcachedHosts => DefaultMemcacheHosts,
-    rcsBindir => DefaultRcsBindir,
-    rcsDir => DefaultRcsDir,
+  do {
+    no strict "refs";
+
+    @{"$caller\::EXPORT_OK"} = ( );
   };
+
+  for my $key ( keys %{$rc} ) {
+    do {
+      no strict "refs";
+      no warnings "once";
+
+      push @{"$caller\::EXPORT_OK"}, $key;
+    };
+
+    eval qq|
+      package $caller;
+
+      use constant \$key => \$rc->{$key};
+    |;
+
+    throw OP::RuntimeError($@) if $@;
+  }
 }
 
-for my $key ( keys %{$rc} ) {
-  push @EXPORT_OK, $key;
-
-  eval qq| use constant $key => \$rc->{$key}; |;
-
-  throw OP::RuntimeError($@) if $@;
-}
+init '.oprc';
 
 =pod
+
+=head1 CUSTOM RC FILES
+
+Developers may create self-standing rc files for application-specific
+consumption. Just use OP::Constants as a base, and invoke C<init> for the
+named rc file.
+
+Just as C<.oprc>, the custom rc file must contain valid YAML, and it lives
+under C<$ENV{OP_HOME}>.
+
+For example, in a hypothetical C<.myapprc>:
+
+  ---
+  hello: howdy
+
+Hypothetical package MyApp/Constants.pm makes any keys available
+as Perl constants:
+
+  package MyApp::Constants;
+
+  use base qw| OP::Constants |;
+
+  OP::Constants::init(".myapprc");
+
+  1;
+
+Callers may consume the constants package, requesting symbols for export:
+
+  use MyApp::Constants qw| hello |;
+
+  say hello;
+
+  #
+  # Prints "howdy"
+  #
 
 =head1 DIAGNOSTICS
 
